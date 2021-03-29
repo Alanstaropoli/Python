532118f1d95e4a3e7e638ed3c3bff5ec223024f8
Alan Staropoli <alan.edlp.st@gmail.com>
2021-03-28 21:16:32 -0300

    Resolucion del ejercicio 7 de la Practica 2.

---
+++ punto7.py
@@ -0,0 +1,196 @@
+string = '''
+'Agustin',
+ 'Alan',
+ 'Andrés',
+ 'Ariadna',
+ 'Bautista',
+ 'CAROLINA',
+ 'CESAR',
+ 'David',
+ 'Diego',
+ 'Dolores',
+ 'DYLAN',
+ 'ELIANA',
+ 'Emanuel',
+ 'Fabián',
+ 'Facundo',
+ 'Facundo',
+ 'FEDERICO',
+ 'FEDERICO',
+ 'GONZALO',
+ 'Gregorio',
+ 'Ignacio',
+ 'Jonathan',
+ 'Jonathan',
+ 'Jorge',
+ 'JOSE',
+ 'JUAN',
+ 'Juan',
+ 'Juan',
+ 'Julian',
+ 'Julieta',
+ 'LAUTARO',
+ 'Leonel',
+ 'LUIS',
+ 'Luis',
+ 'Marcos',
+ 'María',
+ 'MATEO',
+ 'Matias',
+ 'Nicolás',
+ 'NICOLÁS',
+ 'Noelia',
+ 'Pablo',
+ 'Priscila',
+ 'TOMAS',
+ 'Tomás',
+ 'Ulises',
+ 'Yanina'
+ '''
+
+string2 = '''
+ 81,
+ 60,
+ 72,
+ 24,
+ 15,
+ 91,
+ 12,
+ 70,
+ 29,
+ 42,
+ 16,
+ 3,
+ 35,
+ 67,
+ 10,
+ 57,
+ 11,
+ 69,
+ 12,
+ 77,
+ 13,
+ 86,
+ 48,
+ 65,
+ 51,
+ 41,
+ 87,
+ 43,
+ 10,
+ 87,
+ 91,
+ 15,
+ 44,
+ 85,
+ 73,
+ 37,
+ 42,
+ 95,
+ 18,
+ 7,
+ 74,
+ 60,
+ 9,
+ 65,
+ 93,
+ 63,
+ 74
+ '''
+ 
+string3 = '''
+ 30,
+ 95,
+ 28,
+ 84,
+ 84,
+ 43,
+ 66,
+ 51,
+ 4,
+ 11,
+ 58,
+ 10,
+ 13,
+ 34,
+ 96,
+ 71,
+ 86,
+ 37,
+ 64,
+ 13,
+ 8,
+ 87,
+ 14,
+ 14,
+ 49,
+ 27,
+ 55,
+ 69,
+ 77,
+ 59,
+ 57,
+ 40,
+ 96,
+ 24,
+ 30,
+ 73,
+ 95,
+ 19,
+ 47,
+ 15,
+ 31,
+ 39,
+ 15,
+ 74,
+ 33,
+ 57,
+ 10
+ '''
+
+# Creo 3 listas en donde van a estar los nombres de los alumnos y las notas de las 2 evaluaciones.
+nombres = string.split()
+evaluacion1 = string2.split()
+evaluacion2 = string3.split()
+
+# Le saco las "'" y las "," al contenido de las listas y paso los strings con las notas a int.
+for elemento in nombres:
+    i = nombres.index(elemento)
+    for car in elemento:
+        if not car.isalpha():
+            elemento = elemento.replace(car, '')
+    nombres[i] = elemento
+for elemento in evaluacion1:
+    i = evaluacion1.index(elemento)
+    for num in elemento:
+        if not num.isdigit():
+            elemento = elemento.replace(num, ' ')
+    evaluacion1[i] = int(elemento)
+for elemento in evaluacion2:
+    i = evaluacion2.index(elemento)
+    for num in elemento:
+        if not num.isdigit():
+            elemento = elemento.replace(num, ' ')
+    evaluacion2[i] = int(elemento)
+
+# Creo un zip para unificar las notas de las evaluaciones.
+notas = zip(evaluacion1, evaluacion2)
+total_notas = []
+for estructura in notas:
+    total_notas.append(estructura[0]+estructura[1])
+
+# Creo la lista definitiva con los nombres de los estudiantes y sus respectivas notas totales.
+notas = list(zip(nombres, total_notas))
+
+# Calculo el promedio de las notas totales de los alumnos.
+suma = 0
+for elemento in total_notas:
+    suma += elemento
+promedio = suma/len(total_notas)
+print(f'El promedio de las notas totales de los alumnos es de {int(promedio)}.')
+
+# Imprimo el listado de los alumnos con peores notas.    
+print('Listado de alumnos con notas menores al promedio: ')
+for estructura in notas:
+    if estructura[1] < promedio:
+        print(f'{estructura[0]}.')

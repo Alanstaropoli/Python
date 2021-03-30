e78ae87cabdbce0939b5318befe2f82582544a76
Alan Staropoli <alan.edlp.st@gmail.com>
2021-03-30 17:00:03 -0300

    Resolucion del ejercicio 7 de la Practica 2

--- punto7.py
+++ punto7.py
@@ -148,49 +148,63 @@ string3 = '''
  10
  '''
 
-# Creo 3 listas en donde van a estar los nombres de los alumnos y las notas de las 2 evaluaciones.
-nombres = string.split()
-evaluacion1 = string2.split()
-evaluacion2 = string3.split()
+def crear_lista():
 
-# Le saco las "'" y las "," al contenido de las listas y paso los strings con las notas a int.
-for elemento in nombres:
-    i = nombres.index(elemento)
-    for car in elemento:
-        if not car.isalpha():
-            elemento = elemento.replace(car, '')
-    nombres[i] = elemento
-for elemento in evaluacion1:
-    i = evaluacion1.index(elemento)
-    for num in elemento:
-        if not num.isdigit():
-            elemento = elemento.replace(num, ' ')
-    evaluacion1[i] = int(elemento)
-for elemento in evaluacion2:
-    i = evaluacion2.index(elemento)
-    for num in elemento:
-        if not num.isdigit():
-            elemento = elemento.replace(num, ' ')
-    evaluacion2[i] = int(elemento)
+    """ Esta funcion crea una lista y la devuelve con el nombre de los alumnos y sus notas totales. """
+    
+    nombres = string.split()
+    evaluacion1 = string2.split()
+    evaluacion2 = string3.split()
 
-# Creo un zip para unificar las notas de las evaluaciones.
-notas = zip(evaluacion1, evaluacion2)
-total_notas = []
-for estructura in notas:
-    total_notas.append(estructura[0]+estructura[1])
+    # Le saco las "'" y las "," al contenido de las listas y paso los strings con las notas a int.
+    for elemento in nombres:
+        i = nombres.index(elemento)
+        for car in elemento:
+            if not car.isalpha():
+                elemento = elemento.replace(car, '')
+        nombres[i] = elemento
+    for elemento in evaluacion1:
+        i = evaluacion1.index(elemento)
+        for num in elemento:
+            if not num.isdigit():
+                elemento = elemento.replace(num, ' ')
+        evaluacion1[i] = int(elemento)
+    for elemento in evaluacion2:
+        i = evaluacion2.index(elemento)
+        for num in elemento:
+            if not num.isdigit():
+                elemento = elemento.replace(num, ' ')
+        evaluacion2[i] = int(elemento)
 
-# Creo la lista definitiva con los nombres de los estudiantes y sus respectivas notas totales.
-notas = list(zip(nombres, total_notas))
+    # Creo un zip para unificar las notas de las evaluaciones.
+    notas = zip(evaluacion1, evaluacion2)
+    total_notas = []
+    for estructura in notas:
+        total_notas.append(estructura[0]+estructura[1])
 
-# Calculo el promedio de las notas totales de los alumnos.
-suma = 0
-for elemento in total_notas:
-    suma += elemento
-promedio = suma/len(total_notas)
-print(f'El promedio de las notas totales de los alumnos es de {int(promedio)}.')
+    notas = list(zip(nombres, total_notas))
+    return notas
 
-# Imprimo el listado de los alumnos con peores notas.    
-print('Listado de alumnos con notas menores al promedio: ')
-for estructura in notas:
-    if estructura[1] < promedio:
-        print(f'{estructura[0]}.')
+def calcular_promedio(notas):
+    
+    """ Esta funcion calcula y devuelve el promedio de las notas totales de los alumnos. """
+   
+    suma = 0
+    for nota in notas:
+        suma += nota[1]
+    promedio = suma/len(notas)
+    print(f'El promedio de las notas totales de los alumnos es de {int(promedio)}.')
+    return promedio
+
+def notas_bajas(promedio):
+    
+    """ Esta funcion imprime el listado de los alumnos con peores notas. """
+
+    print('Listado de alumnos con notas menores al promedio: ')
+    for estructura in notas:
+        if estructura[1] < promedio:
+            print(f'{estructura[0]}.')
+
+notas = crear_lista()
+promedio = calcular_promedio(notas)
+notas_bajas(promedio)
